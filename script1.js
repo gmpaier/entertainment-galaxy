542109d8eacfc817512e6bd1f528522142ff4b83
grif <gmpaier@loyola.edu>
2021-03-08 20:30:57 -0500

    redid testdex styling with skeleton to ensure script works as expected

--- script1.js
+++ script1.js
@@ -15,10 +15,10 @@ function searchTmdb (){
           console.log(data);
           for (let i = 0; i < data.results.length; i++){
             if (data.results[i].media_type === "movie"){
-              $(".list-group").append('<button type="button" class="list-group-item list-group-item-action short" value="'+ data.results[i].id + '">' + data.results[i].title + ' (' + data.results[i].media_type + ')</button>');
+              $("#result-list").append('<li><button type="button" class="list-group-item list-group-item-action short" value="'+ data.results[i].id + ' media='+ data.results[i].media_type + '">' + data.results[i].title + ' (' + data.results[i].media_type + ')</button></li>');
             }
             else if (data.results[i].media_type === "tv"){
-              $(".list-group").append('<button type="button" class="list-group-item list-group-item-action short" value="'+ data.results[i].id + '">' + data.results[i].name + ' (' + data.results[i].media_type + ')</button>');
+              $("#result-list").append('<li><button type="button" class="list-group-item list-group-item-action short" value="'+ data.results[i].id + ' media='+ data.results[i].media_type + '">' + data.results[i].name + ' (' + data.results[i].media_type + ')</button></li>');
             }
           }
 
@@ -29,6 +29,7 @@ function searchTmdb (){
 function getTmdb (){
     var key = "537dc0254ee4eb9747eecc6e3667403f";
     var id = parseInt(this.value);
+    var media = this.media;
     var requestUrl = "https://api.themoviedb.org/3/movie/" + id + "?api_key=" + key + "&language=en-US";
     fetch(requestUrl)
       .then(function (response) {
@@ -39,14 +40,38 @@ function getTmdb (){
       })
       .then(function (data) {
           console.log(data);
+         if (media === 'movie'){
+          return getMovieMusic(id);
+         }
+         else if (media === 'tv'){
+          return getShowMusic(id);
+         }
+      })
+}   
+    
+function getMovieMusic (id){
+  var requestUrl = "https://cors-anywhere.herokuapp.com/https://45fedfc9.api.tunefind.com/api/v2/movie/" + id + "?id-type=tmdb";
+    fetch(requestUrl, {
+        headers: {
+          authorization: "Basic NDVmZWRmYzkwNjQ1MzEyZjFlNzJkYzZjNGIzNTk5ZjY6MDYxYWY4Mzg5ZmU1MTQ0ZjI0YTE0Y2MxOGMyZmRkNTI=",
+        }
+      })
+    .then(function (response) {
+        if (response.status !== 200){
+            alert("invalid address");
+        }
+
+        return response.json();
+    })
+      .then(function (data) {
+          console.log(data);
       
-          //return getMusic(id);
           return data;
       })
 }   
-    //problem child API doesn't want to recognize TMDb IDs as advertized
-function getMusic (id){
-    var requestUrl = "https://cors-anywhere.herokuapp.com/https://45fedfc9.api.tunefind.com/api/v2/movie/" + id + "?id-type=tmdb";
+
+function getShowMusic (id){
+  var requestUrl = "https://cors-anywhere.herokuapp.com/https://45fedfc9.api.tunefind.com/api/v2/show/" + id + "?id-type=tmdb";
     fetch(requestUrl, {
         headers: {
           authorization: "Basic NDVmZWRmYzkwNjQ1MzEyZjFlNzJkYzZjNGIzNTk5ZjY6MDYxYWY4Mzg5ZmU1MTQ0ZjI0YTE0Y2MxOGMyZmRkNTI=",
