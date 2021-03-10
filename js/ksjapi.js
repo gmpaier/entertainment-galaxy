512dfd170ae0108ed548a9456b5f6f4e9a96d908
Ken Otuo <mrkoajr@gmail.com>
2021-03-10 16:26:34 -0500

    updating the readme file

---
+++ js/ksjapi.js
@@ -0,0 +1,15 @@
+class Fetch {
+    async justWatch(input) {
+      const myKey = ;
+  
+      //make request to url
+  
+      const response = await fetch(
+        ``
+      );
+  
+      const data = await response.json();
+  
+      console.log(data);
+  
+      return data;
